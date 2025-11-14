b419a4187c20d89467d6c0f0a06f4b684348e45a
JoTerrance <gsanz@iespabloserrano.com>
2025-11-14 11:47:42 +0100

    add student manager

--- odoo-compose/addons/courses/models.py
+++ odoo-compose/addons/courses/models.py
@@ -7,8 +7,23 @@ from odoo import models, fields, api
 class Course(models.Model):
     _name = 'course.course'
     _description = 'Course'
+    _rec_name = 'nombre'
 
     nombre = fields.Char(string='Course Name', required=True)
     description = fields.Text(string='Course Description')
     duration = fields.Integer(string='Duration (hours)')
-    active = fields.Boolean(string='Active', default=True)
+    course_act = fields.Boolean(string='Active', default=True)
+    students = fields.One2many('course.student', 'enrolled_courses', string='Enrolled Students')
+    
+    
+
+#create a model called student
+class Student(models.Model):
+    _name = 'course.student'
+    _description = 'Student'
+
+    name = fields.Char(string='Student Name', required=True)
+    age = fields.Integer(string='Age')
+    email = fields.Char(string='Email')
+    enrolled_courses = fields.Many2one('course.course', string='Enrolled Course')
+
